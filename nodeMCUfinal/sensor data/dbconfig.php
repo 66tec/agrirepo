926cbae19309438b599c0813214b41385bb04995
66tec <75468017+66tec@users.noreply.github.com>
2021-03-20 10:10:23 +0500

    finalization v1.0

---
+++ nodeMCUfinal/sensor data/dbconfig.php
@@ -0,0 +1,11 @@
+<?php
+/****************************************************/
+/************** Created by : Vivek Gupta ************/
+/***************     www.vsgupta.in     *************/
+/***************     www.iotmonk.com     *************/
+/****************************************************/ 
+define('DB_USER', "tecozk_agritech2");     // Your database user name
+define('DB_PASSWORD', "agritech2db");			// Your database password (mention your db password here)
+define('DB_DATABASE', "tecozk_agritech2"); // Your database name
+define('DB_SERVER', "localhost");			// db server (Mostly will be 'local' host)
+?>
